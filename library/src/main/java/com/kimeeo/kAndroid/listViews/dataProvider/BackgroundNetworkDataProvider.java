61398aae10532325a69c069c46004f0f0a4273b2
BhavinPadhiyar <mail.kimeeo@gmail.com>
2016-05-02 19:25:13 +0530

    DataModel

---
+++ library/src/main/java/com/kimeeo/kAndroid/listViews/dataProvider/BackgroundNetworkDataProvider.java
@@ -0,0 +1,33 @@
+package com.kimeeo.kAndroid.listViews.dataProvider;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Created by BhavinPadhiyar on 27/04/16.
+ */
+abstract public class BackgroundNetworkDataProvider extends BackgroundDataProvider
+{
+    public BackgroundNetworkDataProvider()
+    {
+
+    }
+
+    public static final int METHOD_POST =1;
+    public static final int METHOD_GET =2;
+    abstract protected int getMethod();
+    abstract protected String getNextURL();
+    abstract protected Map<String,Object> getNextParam();
+    abstract protected Class<DataModel> getDataModel();
+    protected String getRefreshURL() {
+        return null;
+    }
+    protected Map<String,Object> getRefreshParam(){
+        return null;
+    }
+
+    public interface DataModel{
+        public List getDataProvider();
+    }
+}
