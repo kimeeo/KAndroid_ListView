ee7aa00dfd7c07c35b35b8ba49d77c11f5aa5cc4
BhavinPadhiyar <mail.kimeeo@gmail.com>
2016-05-18 17:29:08 +0530

    View corrected

---
+++ app/src/main/java/com/kimeeo/kAndroid/listViews/ListView.java
@@ -0,0 +1,49 @@
+package com.kimeeo.kAndroid.listViews;
+
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kimeeo.kAndroid.listViews.dataProvider.DataProvider;
+import com.kimeeo.kAndroid.listViews.dataProvider.StaticDataProvider;
+import com.kimeeo.kAndroid.listViews.recyclerView.BaseItemHolder;
+/**
+ * Created by BhavinPadhiyar on 02/05/16.
+ */
+public class ListView extends com.kimeeo.kAndroid.listViews.recyclerView.verticalViews.ListView {
+    @Override
+    public View getItemView(int i, LayoutInflater layoutInflater, ViewGroup viewGroup) {
+        return layoutInflater.inflate(R.layout.sample_list_view_item,viewGroup,false);
+    }
+    @Override
+    public BaseItemHolder getItemHolder(int i, View view){
+        return new BaseItemHolder1(view);
+    }
+
+    @NonNull
+    @Override
+    protected DataProvider createDataProvider()
+    {
+        StaticDataProvider data = new StaticDataProvider();
+        return data;
+    }
+    public class StaticDataProvider1 extends StaticDataProvider
+    {
+
+    }
+
+    public class BaseItemHolder1 extends BaseItemHolder
+    {
+        public BaseItemHolder1(View itemView) {
+            super(itemView);
+        }
+        @Override
+        public void updateItemView(Object o, View view, int i) {
+            DataBean data = (DataBean) o;
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(i +". "+data.getName());
+        }
+    }
+}
--- library/src/main/java/com/kimeeo/kAndroid/listViews/dataProvider/StaticDataProvider.java
+++ library/src/main/java/com/kimeeo/kAndroid/listViews/dataProvider/StaticDataProvider.java
@@ -4,6 +4,6 @@ package com.kimeeo.kAndroid.listViews.dataProvider;
  * Created by BhavinPadhiyar on 27/04/16.
  */
 public class StaticDataProvider extends DataProvider {
-    final protected void invokeLoadNext(){};
-    final protected void invokeLoadRefresh(){};
+    protected void invokeLoadNext(){};
+    protected void invokeLoadRefresh(){};
 }
