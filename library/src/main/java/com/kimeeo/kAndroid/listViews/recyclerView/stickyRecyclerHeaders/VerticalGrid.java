b2f18e726900a1c98f836d5a9210971e03ff36f6
BhavinPadhiyar <mail.kimeeo@gmail.com>
2016-05-20 18:28:01 +0530

    Method was wrong

--- library/src/main/java/com/kimeeo/kAndroid/listViews/recyclerView/stickyRecyclerHeaders/VerticalGrid.java
+++ library/src/main/java/com/kimeeo/kAndroid/listViews/recyclerView/stickyRecyclerHeaders/VerticalGrid.java
@@ -9,11 +9,14 @@ import com.kimeeo.kAndroid.listViews.recyclerView.GridHelper;
  * Created by bhavinpadhiyar on 1/30/16.
  */
 abstract public class VerticalGrid extends DefaultStickyHeaderView implements GridHelper.IColoumProvider {
-    protected RecyclerView.LayoutManager getLayoutManager() {
+
+    private GridHelper gridHelper;
+
+    @Override
+    protected RecyclerView.LayoutManager createLayoutManager() {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), getColumnsCount(), GridLayoutManager.VERTICAL, false);
         return gridLayoutManager;
     }
-
 
     public int getColumnsCount()
     {
@@ -25,22 +28,24 @@ abstract public class VerticalGrid extends DefaultStickyHeaderView implements Gr
     public int getColumnsPhone() {
         return 2;
     }
+
     public int getColumnsTablet10() {
         return 5;
     }
+
     public int getColumnsTablet7() {return 4;}
 
     public int getSpanSizeForItem(int position,int viewType,Object baseObject)
     {
         return 1;
     }
+
     protected void garbageCollectorCall() {
         super.garbageCollectorCall();
         if(gridHelper!=null)
             gridHelper.garbageCollectorCall();
         gridHelper=null;
     }
-    private GridHelper gridHelper;
 
     @Override
     protected void configViewParam()
